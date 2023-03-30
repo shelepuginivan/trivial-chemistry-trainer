96881632868e27325a0ab8b827166571cd7960b2
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 01:23:08 +0300

    Added app pages

---
+++ src/pages/Stats.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Stats: FC = () => {
+	return (
+		<main>
+			<h1>Stats</h1>
+		</main>
+	)
+}
+
+export default Stats
---
+++ src/pages/Training.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Training: FC = () => {
+	return (
+		<main>
+			<h1>Training</h1>
+		</main>
+	)
+}
+
+export default Training
