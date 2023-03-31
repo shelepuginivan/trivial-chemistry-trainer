e055a9c02139a711d4bee627b4ce6c8bc10f33ca
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 22:55:18 +0300

    Added QuestionCard styles

--- src/ui/QuestionCard/QuestionCard.tsx
+++ src/ui/QuestionCard/QuestionCard.tsx
@@ -9,8 +9,13 @@ type PropsType = {
 const QuestionCard: FC<PropsType> = ({ answer, question }) => {
 	const [revealed, setRevealed] = useState<boolean>(false)
 
+	const flipCard = () => {
+		setRevealed(prev => !prev)
+		setTimeout(() => setRevealed(false), 3000)
+	}
+
 	return (
-		<div className={styles.card} data-revealed={revealed} onClick={() => setRevealed(prev => !prev)}>
+		<div className={styles.card} data-revealed={revealed} onClick={flipCard}>
 			<div className={styles.questionSide}>{question}</div>
 			<div className={styles.answerSide}>{answer}</div>
 		</div>
