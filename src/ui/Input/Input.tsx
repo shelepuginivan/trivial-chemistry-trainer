9e6f31b702faa88c426971f3be6a79f7dea18bf9
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 21:07:01 +0300

    Added UI component Input

---
+++ src/ui/Input/Input.tsx
@@ -0,0 +1,10 @@
+import { forwardRef, InputHTMLAttributes } from 'react'
+import styles from './input.module.sass'
+
+type PropsType = InputHTMLAttributes<HTMLInputElement>
+
+const Input = forwardRef<HTMLInputElement, PropsType>((props, ref) => (
+	<input className={styles.input} {...props} ref={ref}/>
+))
+
+export default Input
