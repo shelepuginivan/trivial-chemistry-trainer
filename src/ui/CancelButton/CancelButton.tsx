afbac29b3df06ae77b55f4f05d54a9571bbb3737
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 23:14:31 +0300

    Added CancelButton UI component

---
+++ src/ui/CancelButton/CancelButton.tsx
@@ -0,0 +1,7 @@
+import { ButtonHTMLAttributes, FC } from 'react'
+import styles from './cancelButton.module.sass'
+
+const CancelButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) =>
+	<button {...props} className={styles.button} />
+
+export default CancelButton
