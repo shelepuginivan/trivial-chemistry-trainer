738394e8843471e8218f12eaa9fff239cf1b1558
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 20:36:09 +0300

    Added prop `exerciseMap` for displaying correct answers where student made a mistake

--- src/components/TrainingResults/TrainingResults.tsx
+++ src/components/TrainingResults/TrainingResults.tsx
@@ -1,13 +1,16 @@
 import { FC } from 'react'
 import { timeInReadableFormat } from '../../utils/timeInReadableFormat'
+import styles from './trainingResults.module.sass'
 
 type PropsType = {
+	exerciseMap: Map<string, string>
 	correctAnswers: [string, string][]
 	incorrectAnswers: [string, string][]
 	trainingStartTime: number
 }
 
 const TrainingResults: FC<PropsType> = ({
+	exerciseMap,
 	correctAnswers,
 	incorrectAnswers,
 	trainingStartTime,
@@ -17,19 +20,56 @@ const TrainingResults: FC<PropsType> = ({
 
 	const correctAnswersGiven = correctAnswers.length
 	const totalAnswersGiven = correctAnswersGiven + incorrectAnswers.length
-	const correctAnswersPercentage = (100 * correctAnswersGiven / totalAnswersGiven).toFixed(2)
-	const averageAnswerSpeedPerMinutes = (1000 * 60 * totalAnswersGiven / trainingTime).toFixed(2)
+	const correctAnswersPercentage = (
+		(100 * correctAnswersGiven) /
+		totalAnswersGiven
+	).toFixed(2)
+	const averageAnswerSpeedPerMinutes = (
+		(1000 * 60 * totalAnswersGiven) /
+		trainingTime
+	).toFixed(2)
+
+	const mistakeTable =
+		correctAnswersGiven !== totalAnswersGiven ? (
+			<table>
+				<thead>
+					<tr>
+						<td>Тривиальное название</td>
+						<td>Ваш ответ</td>
+						<td>Правильный ответ</td>
+					</tr>
+				</thead>
+				{incorrectAnswers.map((questionAnswerPair, index) => {
+					const [question, answer] = questionAnswerPair
+					const correctAnswer = exerciseMap.get(question) as string
+
+					return (
+						<tr key={index}>
+							<td>{question}</td>
+							<td className={styles.mistake}>{answer}</td>
+							<td className={styles.correct}>{correctAnswer}</td>
+						</tr>
+					)
+				})}
+			</table>
+		) : null
 
 	return (
 		<main>
 			<h1>Результаты</h1>
-			<ul>
-				<li>Время: {trainingTimeInReadableFormat}</li>
-				<li>Всего ответов: {totalAnswersGiven}</li>
-				<li>Верных ответов: {correctAnswersGiven}</li>
-				<li>Процент выполнения: {correctAnswersPercentage} %</li>
-				<li>Средняя скорость ответа: {averageAnswerSpeedPerMinutes} <sup>отв.</sup>/<sub>мин</sub></li>
-			</ul>
+			<div className={styles.trainingStats}>
+				<ul>
+					<li>Время: {trainingTimeInReadableFormat}</li>
+					<li>Всего ответов: {totalAnswersGiven}</li>
+					<li>Верных ответов: {correctAnswersGiven}</li>
+					<li>Процент выполнения: {correctAnswersPercentage} %</li>
+					<li>
+						Средняя скорость: {averageAnswerSpeedPerMinutes}{' '}
+						<sup>отв.</sup>/<sub>мин</sub>
+					</li>
+				</ul>
+				{mistakeTable}
+			</div>
 		</main>
 	)
 }
