5d8c8aa484dcf839a49ae05016f4ec1c006bd080
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 11:22:16 +0300

    Added training page component logic with questions and answers system

--- src/pages/Training.tsx
+++ src/pages/Training.tsx
@@ -1,11 +1,62 @@
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import TrainingResults from '../components/TrainingResults/TrainingResults'
+import { EXERCISES_ROUTE } from '../utils/routes'
+import { EXERCISES_MAPS } from '../utils/trivialNamesMaps'
+import { randomMapItem } from '../utils/randomMapItem'
 
 const Training: FC = () => {
-	return (
-		<main>
-			<h1>Training</h1>
-		</main>
-	)
+	const params = useParams<{ exerciseId: string }>()
+	const exerciseId = Number(params.exerciseId) - 1
+
+	if (isNaN(exerciseId)) return <Navigate to={EXERCISES_ROUTE} />
+
+	const trainingStartTime = useRef(Date.now())
+
+	const currentExerciseMap = EXERCISES_MAPS[exerciseId]
+
+	const [finished, setFinished] = useState<boolean>(false)
+	const [correctAnswers, setCorrectAnswers] = useState<[string, string][]>([['', '']])
+	const [incorrectAnswers, setIncorrectAnswers] = useState<[string, string][]>([['', '']])
+
+	const OngoingTraining: FC = () => {
+		const [initialQuestion, initialCorrectAnswer] = randomMapItem(currentExerciseMap)
+
+		const answerInput = useRef<HTMLInputElement>(null)
+
+		const [question, setQuestion] = useState<string>(initialQuestion)
+		const [correctAnswer, setCorrectAnswer] = useState<string>(initialCorrectAnswer)
+
+		const submit = () => {
+			const receivedAnswer = answerInput.current?.value
+
+			if (!receivedAnswer) return
+
+			;(receivedAnswer === correctAnswer
+				? setCorrectAnswers
+				: setIncorrectAnswers)(
+				(prev) => [...prev, [question, receivedAnswer]]
+			)
+
+			const [newQuestion, newCorrectAnswer] = randomMapItem(currentExerciseMap)
+			setQuestion(newQuestion)
+			setCorrectAnswer(newCorrectAnswer)
+
+			answerInput.current.value = ''
+		}
+
+		return (
+			<main onKeyDown={e => e.code === 'Escape' ? setFinished(true) : {}}>
+				{question}
+				<input autoFocus ref={answerInput} type='text' onKeyDown={e => e.code === 'Enter' ? submit() : {}} />
+			</main>
+		)
+	}
+
+
+	return <>{
+		finished ? <TrainingResults correctAnswers={correctAnswers} incorrectAnswers={incorrectAnswers} trainingStartTime={trainingStartTime.current}/> : <OngoingTraining />
+	}</>
 }
 
 export default Training
