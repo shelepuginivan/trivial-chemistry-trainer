0c372f4737c4ac3b0ab9e7c6cce6868ceaf1c318
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 11:47:51 +0300

    Added more stats

--- src/components/TrainingResults/TrainingResults.tsx
+++ src/components/TrainingResults/TrainingResults.tsx
@@ -15,12 +15,20 @@ const TrainingResults: FC<PropsType> = ({
 	const trainingTime = Date.now() - trainingStartTime
 	const trainingTimeInReadableFormat = timeInReadableFormat(trainingTime)
 
+	const correctAnswersGiven = correctAnswers.length
+	const totalAnswersGiven = correctAnswersGiven + incorrectAnswers.length
+	const correctAnswersPercentage = (100 * correctAnswersGiven / totalAnswersGiven).toFixed(2)
+	const averageAnswerSpeedPerMinutes = (1000 * 60 * totalAnswersGiven / trainingTime).toFixed(2)
+
 	return (
 		<main>
 			<h1>Результаты</h1>
 			<ul>
 				<li>Время: {trainingTimeInReadableFormat}</li>
-				<li></li>
+				<li>Всего ответов: {totalAnswersGiven}</li>
+				<li>Верных ответов: {correctAnswersGiven}</li>
+				<li>Процент выполнения: {correctAnswersPercentage} %</li>
+				<li>Средняя скорость ответа: {averageAnswerSpeedPerMinutes} <sup>отв.</sup>/<sub>мин</sub></li>
 			</ul>
 		</main>
 	)
