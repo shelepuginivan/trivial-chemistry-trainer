7ca04f943541bb517129744dce033dcddc047ef8
shelepuginivan <shelepuginivanm@gmail.com>
2023-03-31 23:14:20 +0300

    Added SubmitButton UI component

---
+++ src/ui/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,7 @@
+import { ButtonHTMLAttributes, FC } from 'react'
+import styles from './submitButton.module.sass'
+
+const SubmitButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) =>
+	<button {...props} className={styles.button} />
+
+export default SubmitButton
