637383ff4fdc712fb5eb00a99c698947c798ce70
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-01 13:27:44 +0300

    Refactored stars, separate each exercise stat to different localStorage keys

--- src/utils/Stats.ts
+++ src/utils/Stats.ts
@@ -1,15 +1,61 @@
+import { EXERCISES_NAMES_MAPPING } from './trivialNamesMaps'
+
 export class Stats {
+	private static _statStorageKey(id: number) {
+		return `stats_${id}`
+	}
+
+	static initializeStorage(): void {
+		try {
+			for (let i = 1; i <= EXERCISES_NAMES_MAPPING.length; i++) {
+				const key = this._statStorageKey(i)
+				const statsOfExerciseById = localStorage.getItem(key)
+
+				if (!statsOfExerciseById) {
+					localStorage.setItem(key, '[]')
+				}
+			}
+		} catch {}
+	}
+
 	static getAllStats(): number[][] {
 		try {
-			return JSON.parse(localStorage.getItem('stats') || '[]') as number[][]
+			const allStats = []
+
+			for (let i = 1; i <= EXERCISES_NAMES_MAPPING.length; i++) {
+				const key = this._statStorageKey(i)
+				const statsOfExerciseById = localStorage.getItem(key)
+
+				if (!statsOfExerciseById) {
+					allStats.push([])
+					continue
+				}
+
+				allStats.push(JSON.parse(statsOfExerciseById))
+			}
+
+			return allStats
 		} catch {
 			return []
 		}
 	}
 
 	static getStatsOfExerciseById(id: number): number[] {
-		const stats = this.getAllStats()
-		return stats[id] || []
+		try {
+			const stats = localStorage.getItem(this._statStorageKey(id))
+
+			if (!stats) return []
+
+			const parsedStats = JSON.parse(stats)
+			const lastItem = parsedStats[parsedStats.length - 1]
+
+			if (typeof lastItem !== 'number' && isNaN(Number(lastItem)))
+				return []
+
+			return parsedStats
+		} catch {
+			return []
+		}
 	}
 
 	static getLastResultOfExerciseById(id: number): number {
@@ -17,15 +63,29 @@ export class Stats {
 		return statsOfThisExercise[statsOfThisExercise.length - 1] || 0
 	}
 
-	static addResultToExerciseStats(id: number, resultInPercents: number): void {
+	static addResultToExerciseStats(
+		id: number,
+		resultInPercents: number
+	): void {
 		try {
-			const stats = this.getAllStats()
+			const statsOfExerciseById = this.getStatsOfExerciseById(id)
 
-			stats[id].push(resultInPercents)
+			statsOfExerciseById.push(resultInPercents)
 
-			localStorage.setItem('stats', JSON.stringify(stats))
+			localStorage.setItem(
+				this._statStorageKey(id),
+				JSON.stringify(statsOfExerciseById)
+			)
 		} catch {}
 	}
 
-	static clearStats(): void {localStorage.removeItem('stats')}
+	static clearStats(): void {
+		try {
+			for (let i = 1; i <= EXERCISES_NAMES_MAPPING.length; i++) {
+				const key = this._statStorageKey(i)
+
+				localStorage.setItem(key, '[]')
+			}
+		} catch {}
+	}
 }
