6f326667911fff8b853f1f5c198de921a7954c14
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-02 01:19:51 +0300

    Added Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { HOME_ROUTE } from '../../utils/routes'
+
+const Logo: FC = () => (
+	<Link draggable={false} to={HOME_ROUTE}>
+		<img height={40} draggable={false} src='/favicon.png' alt='logo' />
+	</Link>
+)
+
+export default Logo
