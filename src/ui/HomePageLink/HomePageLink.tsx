e4dab7d86094277468acf8114dc5c651dc52d185
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-02 01:24:57 +0300

    Added UI component `HomePageLink`

---
+++ src/ui/HomePageLink/HomePageLink.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import { Link, LinkProps } from 'react-router-dom'
+import styles from './homePageLink.module.sass'
+
+const HomePageLink: FC<LinkProps> = (props) =>
+	<Link className={styles.link} {...props}/>
+
+export default HomePageLink
