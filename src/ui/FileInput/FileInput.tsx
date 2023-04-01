e543e3ac970e387726d05877c170519677685e41
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-01 20:25:38 +0300

    Added UI component `FileInput`

---
+++ src/ui/FileInput/FileInput.tsx
@@ -0,0 +1,18 @@
+import { FC, InputHTMLAttributes } from 'react'
+import styles from './fileInput.module.sass'
+
+type PropsType = {
+	label: string
+	id: string
+} & Omit<InputHTMLAttributes<HTMLInputElement>, 'id' | 'type'>
+
+const FileInput: FC<PropsType> = ({ id, label, ...inputProps }) => {
+	return (
+		<div className={styles.inputWrapper}>
+			<label htmlFor={id}>{label}</label>
+			<input type='file' id={id} {...inputProps} />
+		</div>
+	)
+}
+
+export default FileInput
