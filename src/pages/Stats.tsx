43e6eef8e72b393d1649443a75e0ab011896a2f7
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-01 18:03:01 +0300

    Added StatsMenu component on Stats page

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react'
 import ExerciseStats from '../components/ExerciseStats/ExerciseStats'
 import { EXERCISES_NAMES_MAPPING } from '../utils/trivialNamesMaps'
+import StatsMenu from '../components/StatsMenu/StatsMenu'
 
 const Stats: FC = () => {
 	return (
 		<main className='mainContainer'>
 			<h1>Статистика</h1>
-
+			<StatsMenu/>
 			{
 				EXERCISES_NAMES_MAPPING.map((title, index) => (
 					<ExerciseStats key={title} exerciseId={index}/>
