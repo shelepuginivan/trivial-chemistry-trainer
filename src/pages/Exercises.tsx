7809d3b881e01f926b2f183077b9122b96492914
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-01 21:48:58 +0300

    Added exercises

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -6,24 +6,28 @@ import { Stats } from '../utils/Stats'
 import { EXERCISES_NAMES_MAPPING } from '../utils/trivialNamesMaps'
 
 const Exercises: FC = () => {
+	const exerciseCardById = (id: number) => {
+		return (
+			<ExerciseCard
+				key={id}
+				linkToExercise={exerciseLink(id)}
+				exerciseTitle={EXERCISES_NAMES_MAPPING[id]}
+				previousResultInPercents={Stats.getLastResultOfExerciseById(id)}
+			/>
+		)
+	}
+
 	return (
 		<main className='mainContainer'>
 			<h1>Упражнения</h1>
 
 			<ExerciseBlock blockName='Общее'>
-				{Array(3).fill(0).map((_, index) => (
-					<ExerciseCard
-						key={index}
-						linkToExercise={exerciseLink(index)}
-						exerciseTitle={EXERCISES_NAMES_MAPPING[index]}
-						previousResultInPercents={Stats.getLastResultOfExerciseById(index)}
-					/>
-				))}
+				{[0, 1, 2].map((_, index) => exerciseCardById(index))}
 			</ExerciseBlock>
 
-			<ExerciseBlock blockName='Нерганическая химия'></ExerciseBlock>
-
-			<ExerciseBlock blockName='Органическая химия'></ExerciseBlock>
+			<ExerciseBlock blockName='Нерганическая химия'>
+				{[3, 4, 5, 6, 7].map((index) => exerciseCardById(index))}
+			</ExerciseBlock>
 		</main>
 	)
 }
