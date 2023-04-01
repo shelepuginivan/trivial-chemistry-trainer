8e723280a60454249b3cf14e14e83a3842265e9a
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-01 15:32:41 +0300

    Added exercises in first exercise block

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -1,13 +1,28 @@
 import { FC } from 'react'
 import ExerciseBlock from '../components/ExerciseBlock/ExerciseBlock'
+import ExerciseCard from '../ui/ExerciseCard/ExerciseCard'
+import { exerciseLink } from '../utils/exerciseLink'
+import { Stats } from '../utils/Stats'
+import { EXERCISES_NAMES_MAPPING } from '../utils/trivialNamesMaps'
 
 const Exercises: FC = () => {
 	return (
 		<main className='mainContainer'>
 			<h1>Упражнения</h1>
 
-			<ExerciseBlock blockName='Общее'></ExerciseBlock>
+			<ExerciseBlock blockName='Общее'>
+				{Array(3).fill(0).map((_, index) => (
+					<ExerciseCard
+						key={index}
+						linkToExercise={exerciseLink(index)}
+						exerciseTitle={EXERCISES_NAMES_MAPPING[index]}
+						previousResultInPercents={Stats.getLastResultOfExerciseById(index)}
+					/>
+				))}
+			</ExerciseBlock>
+
 			<ExerciseBlock blockName='Нерганическая химия'></ExerciseBlock>
+
 			<ExerciseBlock blockName='Органическая химия'></ExerciseBlock>
 		</main>
 	)
